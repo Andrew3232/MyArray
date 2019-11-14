9313a51d261d3f8156dd0d2d74998d2f3b8aea70
Андрій Олейнич <oleinitch@gmail.com>
2019-11-14 04:16:10 +0200

    ,

--- Array/MyArray.h
+++ Array/MyArray.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <iostream>
+#include <string>
 
+using namespace std;
 template<class T>
 class MyArray
 {
@@ -8,14 +11,37 @@ private:
 	int CountRows, CountCols;
 public:
 	//methods
-	int GetCountRows();
-	int GetCountCols();
-	T** GetData();
-	int SetCountRows(int NewCountRows);
-	int SetCountCols(int NewCountCols);
-	int SetData(double** newData, int NewCountRows, int NewcountCols);
-	int SetElement(int IndexRows, int IndexCols, T Value);
-	double GetElement(int indexRows, int IndexCols);
+	int GetCountRows() {
+		return CountRows;
+	}
+	int GetCountCols() {
+		return CountCols;
+	}
+	T GetElement(int indexRows, int IndexCols) {
+		return Data[indexRows][IndexCols];
+	};
+
+	T** GetData() {
+		return Data;
+	};
+
+	void SetCountRows(int NewCountRows) {
+		CountRows = NewCountRows;
+	};
+	void SetCountCols(int NewCountCols) {
+		CountCols = NewCountCols;
+	};
+
+	void SetData(T** newData, int NewCountRows, int NewcountCols) {
+		for(int i = 0; i < CountRows; i++)
+			delete Data[i];
+		delete Data;
+		Data = newData;		
+	};
+
+	void SetElement(int IndexRows, int IndexCols, T Value) {
+		Data[IndexRows][IndexCols] = Value;
+	};	
 
 	//construct
 	MyArray(int NewCountRows = 1, int NewCountCols = 1) {
@@ -34,8 +60,27 @@ public:
 		CountRows = NewCountRows;
 		CountCols = NewCountCols;
 	};
-	MyArray(MyArray& a);
-	~MyArray();
+	MyArray(MyArray& a) {
+		//cout << "In CopyConstructor";
+		Data = new T * [a.GetCountRows()];
+		if (Data == nullptr)
+			exit(0);
+		for (int i = 0; i < a.GetCountRows(); i++) {
+			Data[i] = new T[a.GetCountCols()];
+			if (Data[i] == nullptr) {
+				for (int j = 0; j < i; j++)
+					delete Data[j];
+				delete Data;
+				exit(0);
+			}
+		}
+		CountRows = a.GetCountRows();
+		CountCols = a.GetCountRows();
+		for (int i = 0; i < CountRows; i++)
+			for (int j = 0; j < CountCols; j++)
+				Data[i][j] = a.GetElement(i, j);
+	};
+	~MyArray() {};
 
 	//operators
 	friend MyArray operator+(MyArray& a, MyArray& b);
@@ -52,15 +97,30 @@ public:
 	bool operator==(MyArray& a);
 
 	//cin & cout
-	/*friend ostream& operator<<(ostream& stream, MyArray<T>& A) {
-
-	}
-
+	friend ostream& operator<<(ostream& stream, MyArray<T>& A) {
+		cout << "Arr[" << A.GetCountRows() << "][" << A.GetCountCols() << "] = " << endl;
+		for (int i = 0; i < A.GetCountRows(); i++) {
+			for (int j = 0; j < A.GetCountCols(); j++) {
+				stream << A.GetElement(i, j) << " ";
+			}
+			stream << endl;
+		}
+		return stream;
+	};	
 	friend istream& operator>>(istream& stream, MyArray<T>& A) {
-
-	}*/
+		cout << "Enter array:\n";
+		T buffer;
+		for (int i = 0; i < A.GetCountRows(); i++) {
+			cout << "Row(" << i << ")\n";
+			for (int j = 0; j < A.GetCountCols(); j++) {
+				stream >> buffer;
+				A.SetElement(i, j, buffer);
+			}
+		}
+		return stream;
+	};
 };
-
+/*
 template <typename T>
 MyArray<T>::MyArray(MyArray<T>& a) {
 	//cout << "In CopyConstructor";
@@ -82,4 +142,4 @@ MyArray<T>::MyArray(MyArray<T>& a) {
 		for (int j = 0; j < CountCols; j++)
 			Data[i][j] = a.GetElement(i, j);
 }
-
+*/
